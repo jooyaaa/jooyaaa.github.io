4d86a9c43039ecf937c98993a5ce9f4847bfbf08
jooyaaa <114649320+jooyaaa@users.noreply.github.com>
2022-12-21 09:57:22 +0900

    Delete Communit-page directory

--- Communit-page/js/file-btn.js
+++
@@ -1,7 +0,0 @@
-const fileupload = document.getElementById('file-label')
-const inputValue = document.getElementById('inputChange')
-        
-fileupload.addEventListener('change',function () {
-    console.log(fileupload.value)
-    inputValue.value=fileupload.value;
-})
